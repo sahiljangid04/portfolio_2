bda4cc7311370c618a9bbf73a58c1db6d0eb7464
sahiljangid09 <sahiljangid09@gmail.com>
2026-07-14 23:18:31 +0530

    updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -65,7 +65,7 @@ export const projects: Project[] = [
       {
         id: "resume-2",
         type: "image",
-        src: "/images/projects/1/Screenshot-2.png",
+        src: "/images/projects/1/Screenshot-1.png",
         alt: "SHAP feature importance plot",
       },
       {
